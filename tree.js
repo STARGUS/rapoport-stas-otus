c07a3799eed1424d7dd48dfbf7b63d5015b34655
Stanislav <stanislavmorr@gmail.com>
2022-06-22 15:31:57 +0600

    update logic tree code

--- tree.js
+++ tree.js
@@ -1,57 +1,37 @@
 "use strict";
-const tree = {
+const Tree = {
   name: 1,
   items: [
     {
       name: 2,
-      items: [
-        { name: 3 },
-        { name: 4 },
-        {
-          name: 11,
-          items: [{ name: 12 }, { name: 13 }],
-        },
-      ],
+      items: [{ name: 3 }, { name: 4 }],
     },
     {
-      name: 5,
+      name: 4,
       items: [{ name: 6 }],
     },
   ],
 };
 
-function getTree(tree, index, slip) {
-  let newIndex = ++index;
-  for (let [key, el] of tree.entries()) {
-    if (!!el.items) {
-      levelTree(el, newIndex);
-    } else {
-      if (key != tree.length - 1) {
-        console.log(slip, " ├── ", el.name);
-      } else {
-        console.log(slip, " └── ", el.name);
-      }
-    }
-  }
+function getTree(tree, prefix = "") {
+  let result = "";
+  tree.map((item, index) => {
+    const lastSimbolActive = index === tree.length - 1;
+    result += prefix + (lastSimbolActive ? "└──" : "├──") + item.name + "\n";
+    if (!!item.items)
+      result += getTree(
+        item.items,
+        prefix + (lastSimbolActive ? "" : "|") + "   "
+      );
+  });
+  return result;
 }
-function levelTree(tree, index = 0) {
-  let indexSlip = "";
-  let major = "";
-  for (let i = 0; i < index; i++) {
-    indexSlip = indexSlip + "   ";
-    if (i > 0) {
-      major = major + "    ";
-    }
-  }
-  if (index == 0) {
-    console.log(tree.name);
-  } else {
-    // console.log(major, "├──", tree.name);
-    console.log(major, "└──", tree.name);
-  }
+function start(tree) {
+  let dataTree = "";
+  dataTree += tree.name + "\n";
   if (!!tree.items) {
-    getTree(tree.items, index, indexSlip);
+    dataTree += getTree(tree.items);
   }
+  console.log(dataTree);
 }
-
-levelTree(tree);
+start(Tree);
