33257ae611d11e63abcdaafbac81aca4a306edbd
Stanislav <stanislavmorr@gmail.com>
2022-10-25 07:47:25 +0300

    update role.guard

--- node/node-7/src/guards/role.guard.ts
+++ node/node-7/src/guards/role.guard.ts
@@ -6,10 +6,12 @@ import { Observable } from 'rxjs';
 export class RoleGuard implements CanActivate {
   constructor(private readonly _reflector: Reflector) {}
   async findRole(arr, user) {
+    //Функция проверки наличия роли у пользователя
     return user.role?.map((el) => el.name).some((el) => arr.includes(el));
   }
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const roles = this._reflector.get<string[]>('roles', context.getHandler());
+    roles.push('ADMIN'); //Разрешаем доступ Администратору ко всему контенту
     const req = context.switchToHttp().getRequest();
     if (!!req.user) {
       const user = req.user;
