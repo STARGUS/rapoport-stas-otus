b6a77bb27ede7f7c300d9e8db9da0c9fde18ce19
Stanislav <stanislavmorr@gmail.com>
2022-11-13 18:48:30 +0300

    update pm2

--- node/node-7/src/app.controller.ts
+++ node/node-7/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Render, Response } from '@nestjs/common';
+import { Controller, Get, Post, Render, Response, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import express from 'express';
 import { join } from 'path';
@@ -7,9 +7,11 @@ import { join } from 'path';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // @Get()
-  // //@Render('index')
-  // root(@Response() res) {
-  //  return res.sendfile(join(__dirname, 'client', 'index.html'));
-  // }
+  @Get(':param')
+  //@Render('index')
+  root(@Response() res, @Param('param') param: string) {
+    if (!param.includes('api/'))
+      return res.sendfile(join(__dirname, 'client', 'index.html'));
+    return;
+  }
 }
